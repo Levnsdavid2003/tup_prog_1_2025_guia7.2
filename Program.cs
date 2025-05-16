971ac11a9da887b1624edfeba47641c9880f553e
David L. <162506643+Levnsdavid2003@users.noreply.github.com>
2025-05-16 18:29:20 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrdenDeMenor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor, menor, orden;
+
+            Console.WriteLine("Ingrese el primer número");
+            valor = Convert.ToInt32(Console.ReadLine());
+            menor = valor;
+            orden = 1;
+
+            Console.WriteLine("Ingrese el segundo número");
+            valor = Convert.ToInt32(Console.ReadLine());
+            if (valor < menor) menor = valor;
+            orden++;
+
+            Console.WriteLine("Ingrese el tercer número");
+            valor = Convert.ToInt32(Console.ReadLine());
+            if (valor > menor) menor = valor;
+            orden++;
+
+            Console.WriteLine("Ingrese el cuarto número");
+            valor = Convert.ToInt32(Console.ReadLine());
+            if (valor > menor) menor = valor;
+            orden++;
+
+            Console.WriteLine("Ingrese el último número");
+            valor = Convert.ToInt32(Console.ReadLine());
+            orden++;
+
+            Console.WriteLine("El orden del menor número que se ingresó fué: Orden " + orden);
+            Console.ReadKey();
+        }
+    }
+}
