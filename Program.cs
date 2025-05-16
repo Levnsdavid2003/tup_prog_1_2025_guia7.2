f5208af5cc44f4874805a4834d5a3dbac150c5c6
David L. <162506643+Levnsdavid2003@users.noreply.github.com>
2025-05-16 18:24:48 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PositivoNegativoCero
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Ingrese un número");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            if (num < 0) { Console.WriteLine("El número es Negativo"); }
+            else
+            {
+                if (num == 0) { Console.WriteLine("El número es Cero"); }
+                else { Console.WriteLine("El número es Positivo"); }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
